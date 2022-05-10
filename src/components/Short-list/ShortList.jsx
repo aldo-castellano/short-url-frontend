613964f842c284b9a344cbccb00443ce25a68f59
aldo <aldofcastellanop@gmail.com>
2022-05-10 19:09:15 +0200

    finish styles

---
+++ src/components/Short-list/ShortList.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+const ShortList = ({ url }) => {
+  const [copy, setCopy] = useState(false);
+
+  return (
+    <>
+      <div className=" your-shorter ">
+        <div className="your-shorter-tittle">
+          <h2>your cuts</h2>
+        </div>
+        <div className="item-short">
+          {copy ? (
+            <span className="copy-on copy-ligth"></span>
+          ) : (
+            <span className="copy-off copy-ligth"></span>
+          )}
+          <div className="item-url">
+            <p>{url.original}</p>
+
+            <a target="_blank" href={url.short}>
+              {url.short}
+            </a>
+          </div>
+          <CopyToClipboard text={url.short} onCopy={() => setCopy(true)}>
+            <button className="button button-copy">copy</button>
+          </CopyToClipboard>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ShortList;
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import postUrl from "../helper/postUrl";
 import validator from "validator";
 import managerUrl from "../helper/managerUrl";
+import ShortList from "../components/Short-list/ShortList";
 
 const shortid = require("shortid");
 
@@ -9,6 +10,7 @@ const Main = () => {
   const [url, setUrl] = useState("");
   const [valid, setValid] = useState(false);
   const [shortUrl, setShortUrl] = useState();
+  const [copy, setCopy] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,7 +18,7 @@ const Main = () => {
     if (validator.isURL(url)) {
       const data = await postUrl(url);
 
-      setShortUrl(data.short);
+      setShortUrl(data);
       setUrl("");
     } else {
       setValid(true);
@@ -27,34 +29,27 @@ const Main = () => {
     setValid(false);
   };
 
-  const validatorUrl = () => {
-    if (validator.isURL(url)) {
-      setValid(true);
-    } else {
-      setValid(false);
-    }
-  };
-
   return (
-    <div>
-      <h1>shorter web</h1>
-      <div className="input-shorted">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={url}
-            id="url"
-            onChange={handleChange}
-          ></input>
-          <button>uuid</button>
-          {!valid ? null : <p>url no valida</p>}
-        </form>
+    <div className="container">
+      <div>
+        <h1 className="logo">shortener url</h1>
+        <div>
+          <form className="form-shorter" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={url}
+              placeholder="Escribe tu URL aqui"
+              id="url"
+              className="input-shorter"
+              onChange={handleChange}
+            ></input>
+            <button className=" button button-shorter">cut off</button>
+            {!valid ? null : <p className="error-input">url no valida</p>}
+          </form>
+        </div>
       </div>
-      {shortUrl ? (
-        <a href={shortUrl} target="_blank">
-          {shortUrl}
-        </a>
-      ) : null}
+
+      {shortUrl ? <ShortList url={shortUrl} copy={setCopy}></ShortList> : null}
     </div>
   );
 };
